f50e52d9b55cd8041894f06d04a210d0984f3585
Bruce Ma <39958921+tengcode@users.noreply.github.com>
2023-12-06 20:44:10 +0800

    Update cat_tool.py

--- cat_tool.py
+++ cat_tool.py
@@ -1,1 +1,15 @@
+import sys
+from PySide6 import QtWidgets
+from script import cat_tool
 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    my_window = cat_tool.MyWidget()
+    style_file = './script/tool.qss'
+    # style_sheet = QSSLoader.read_qss_file(style_file)
+    with open(style_file, 'r', encoding='UTF-8') as file:
+        my_window.setStyleSheet(file.read())
+
+    my_window.resize(700, 500)
+    my_window.show()
+    sys.exit(app.exec())
