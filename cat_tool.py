403b9dc424bbb78040c3d86830b86fddc9b4e9b6
Bruce Ma <39958921+tengcode@users.noreply.github.com>
2023-12-06 20:50:05 +0800

    Update cat_tool.py

--- cat_tool.py
+++ cat_tool.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     my_window = cat_ui.MyWidget()
     style_file = './script/tool.qss'
-    # style_sheet = QSSLoader.read_qss_file(style_file)
     with open(style_file, 'r', encoding='UTF-8') as file:
         my_window.setStyleSheet(file.read())
 
